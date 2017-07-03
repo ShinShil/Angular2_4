7b5955503d1e54bb3322b75cc4b7b6ccc91a84a3
ShinShil <Starostin.450503@gmail.com>
2017-07-03 20:58:04 +0300

    part 3 course project

---
+++ ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes:Recipe[] = [
+    new Recipe('recipe 1', 'simple desc 1', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_2CAqyCXQmXjqazz1Ge3SUkv-cRock-wZgrhvEF0Is9xRJejR'),
+    new Recipe('recipe 1', 'simple desc 1', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_2CAqyCXQmXjqazz1Ge3SUkv-cRock-wZgrhvEF0Is9xRJejR')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
