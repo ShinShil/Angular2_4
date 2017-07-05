c8b86a21c535edcb31dcad536522815eed9b97f5
Nikita Starostin <n.starostin@itransition.com>
2017-07-05 12:30:55 +0300

    attribute custom directives example

---
+++ directives-start/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BasicHighloghtDirective } from './basic-highlight/basic-hightlight.directive';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicHighloghtDirective,
+    BetterHighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
