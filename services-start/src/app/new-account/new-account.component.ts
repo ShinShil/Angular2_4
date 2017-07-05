62c4320e9d37b586e69e7275d706867e6c257efa
Nikita Starostin <n.starostin@itransition.com>
2017-07-05 14:38:23 +0300

    Example of wrong service usage

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from './../shared/logging.service';
 
 @Component({
   selector: 'app-new-account',
@@ -12,7 +13,8 @@ export class NewAccountComponent {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    }); 
+    const logService = new LoggingService();
+    logService.logStatusChange(accountStatus);
   }
 }
