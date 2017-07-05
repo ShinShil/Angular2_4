2c9860da254bdadfd21f508e6d029b7db45f54a9
Nikita Starostin <n.starostin@itransition.com>
2017-07-05 14:22:02 +0300

    Maximilian solution directive for toggle dropdown

--- ng4-complete-guide/src/app/shared/dropdown.directive.ts
+++ ng4-complete-guide/src/app/shared/dropdown.directive.ts
@@ -3,21 +3,10 @@ import { Directive, ElementRef, HostBinding, HostListener, Renderer2, OnInit } f
 @Directive({
   selector: '[appDropdown]'
 })
-export class DropdownDirective implements OnInit {
-  dropdownClass = 'open';
-  added = false;
-  parent: any;
-  @HostListener('click') mouseclick() {
-    this.added ?
-      this.renderer.removeClass(this.parent, this.dropdownClass) :
-      this.renderer.addClass(this.parent, this.dropdownClass);
-    this.added = !this.added;
-  }
-  constructor(private elRef: ElementRef, private renderer: Renderer2) {
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
 
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
   }
-  ngOnInit(): void {
-     this.parent = this.renderer.parentNode(this.elRef.nativeElement);
-  }
-
 }
