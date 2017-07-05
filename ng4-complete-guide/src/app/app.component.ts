596356d7424b6ccdcbd11d1cdbc4174750d1111e
Nikita Starostin <n.starostin@itransition.com>
2017-07-05 09:56:16 +0300

    my version of navigation

--- ng4-complete-guide/src/app/app.component.ts
+++ ng4-complete-guide/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  recipesPage = 'app-recipes';
+  shoppingPage = 'app-shopping-list';
+  activePage = this.recipesPage;
+  onMenuItemClicked(activePage: string) {
+    this.activePage = activePage;
+  }
 }
--- ng4-complete-guide/src/app/header/header.component.ts
+++ ng4-complete-guide/src/app/header/header.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: 'header.component.html'
 })
 export class HeaderComponent {
-
+    @Input() recipesPage: string;
+    @Input() shoppingPage: string;
+    @Output() menuItemClickEvent = new EventEmitter<string>();
+    @Input() activePage: string;
+    onMenuItemClick(activePage: string) {
+        this.menuItemClickEvent.emit(activePage);
+    }
 }
