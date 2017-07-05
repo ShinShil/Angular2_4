5069f8c5c9564c6a828cc5352384e34267b65ae5
Nikita Starostin <n.starostin@itransition.com>
2017-07-05 13:36:19 +0300

    creating structured directive

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 import { BasicHighloghtDirective } from './basic-highlight/basic-hightlight.directive';
+import { UnlessDirective } from './unless/unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighloghtDirective,
-    BetterHighlightDirective
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
