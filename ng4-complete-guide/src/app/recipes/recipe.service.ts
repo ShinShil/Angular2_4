817daffb4da5211f33a0af1f5efc23f972e93450
Nikita Starostin <n.starostin@itransition.com>
2017-07-06 10:55:09 +0300

    Add send to shopping list functionality

--- ng4-complete-guide/src/app/recipes/recipe.service.ts
+++ ng4-complete-guide/src/app/recipes/recipe.service.ts
@@ -1,13 +1,38 @@
-import { EventEmitter } from '@angular/core';
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { Ingredient } from './../shared/ingredient.model';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
+
+@Injectable()
 export class RecipeService {
     selectedRecipe = new EventEmitter<Recipe>();
+
+    constructor(private shoppingListService: ShoppingListService) {}
+
     private recepies: Recipe[] = [
-        new Recipe('recipe 1', 'simple desc 1', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_2CAqyCXQmXjqazz1Ge3SUkv-cRock-wZgrhvEF0Is9xRJejR'),
-        new Recipe('recipe 2', 'simple desc 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_2CAqyCXQmXjqazz1Ge3SUkv-cRock-wZgrhvEF0Is9xRJejR')
+        new Recipe(
+            'Fish with potatoe',
+            'What else you need to say',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_2CAqyCXQmXjqazz1Ge3SUkv-cRock-wZgrhvEF0Is9xRJejR',
+            [
+                new Ingredient('Fish', 2),
+                new Ingredient('Potatoe', 20)
+            ]),
+        new Recipe(
+            'Student dinner',
+            'Amazing dish is waiting for you',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_2CAqyCXQmXjqazz1Ge3SUkv-cRock-wZgrhvEF0Is9xRJejR',
+            [
+                new Ingredient('bread', 1),
+                new Ingredient('milk', 2)
+            ])
     ];
 
     getRecepies() {
         return this.recepies;
+    }
+
+    addIngredientToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
     }
 }
--- ng4-complete-guide/src/app/shopping-list/shopping-list.service.ts
+++ ng4-complete-guide/src/app/shopping-list/shopping-list.service.ts
@@ -1,7 +1,7 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListService {
-    ingredientAdded = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('apple', 5),
@@ -10,10 +10,18 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientAdded.emit(this.ingredients);
+        this.ingredientsChanged.emit(this.ingredients);
     }
 
     getIngredients() {
         return this.ingredients.slice();
     }    
+
+    addIngredients(ingredients: Ingredient[]) {
+        // for(const ingredient of ingredients) {
+        //     this.addIngredient(ingredient); //each time will call event emitter, better to add al ingredietns and than cal emit one time
+        // }
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients);
+    }
 }
