b46e8c54e6b350cd0c5c8720062719300a22ef9f
Nikita Starostin <n.starostin@itransition.com>
2017-07-07 13:37:39 +0300

    print data in template

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -10,11 +10,35 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    gender: '',
+    answer: ''
+  }
+
+  submitted = false;
 
   genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: 'suggestedName',
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    this.signupForm.form.patchValue({
+      userData: {
+        userName: 'Suggested Name'
+      }
+    })
   }
 
   // onSubmit(form: NgForm) {
@@ -22,6 +46,13 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.gender = this.signupForm.value.gender;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.submitted = true;
+
+    this.signupForm.reset();
   }
 }
