a0d14e9b509090107ebda0989d4135e24cfbd0a7
Nikita Starostin <n.starostin@itransition.com>
2017-07-11 11:30:23 +0300

    add and remove animation with void keyword

--- ng4-animations-start/src/app/app.component.ts
+++ ng4-animations-start/src/app/app.component.ts
@@ -1,67 +1,98 @@
 import { Component } from '@angular/core';
 import {
-	trigger,
-	state,
-	style,
-	transition,
-	animate
+  trigger,
+  state,
+  style,
+  transition,
+  animate
 } from '@angular/animations';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	animations: [
-		trigger('divState', [
-			state('normal', style({
-				'background-color': 'red',
-				transform: 'translateX(0)'
-			})),
-			state('highlighted', style({
-				'background-color': 'blue',
-				transform: 'translateX(100px)'
-			})),
-			transition('normal <=> highlighted', animate(300)),
-			// transition('normal => highlighted', animate(300)),
-			// transition('highlighted => normal', animate(800)) 
-		]),
-		trigger('wildState', [
-			state('normal', style({
-				'background-color': 'red',
-				transform: 'translateX(0)'
-			})),
-			state('highlighted', style({
-				'background-color': 'blue',
-				transform: 'translateX(100px)'
-			})),
-			state('shrunken', style({
-				'background-color': 'green',
-				transform: 'translateX(0) scale(0.5)'
-			})),			
-			transition('normal => highlighted', animate(300)),
-			transition('highlighted => normal', animate(800)),
-			transition('shrunken <=> *', animate(500))
-		])
-	]
-})
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal <=> highlighted', animate(300)),
+      // transition('normal => highlighted', animate(300)),
+      // transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)',
+        borderRadius: '0'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)',
+        borderRadius: '0'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)',
+        borderRadius: '0'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          'background-color': 'orange',
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ])
+    ]),
+    trigger('listOne', [
+      state('in', style({
+        opacity: '1',
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translate(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: '0'
+        }))]
+      )
+    ])
+  ]})
+  
+
 export class AppComponent {
-	state = 'normal';
-	wildState = 'normal';
-	list = ['Milk', 'Sugar', 'Bread'];
+    state = 'normal';
+    wildState = 'normal';
+    list =['Milk', 'Sugar', 'Bread'];
 
-	onAdd(item) {
-		this.list.push(item);
-	}
+    onAdd(item) {
+      this.list.push(item);
+    }
 
-	onAnimate() {
-		this.state = this.state === 'normal' ? 'highlighted' : 'normal';
-		this.wildState = this.wildState === 'normal' ? 'highlighted' : 'normal';
-	}
+  onAnimate() {
+      this.state = this.state === 'normal' ? 'highlighted' : 'normal';
+      this.wildState = this.wildState === 'normal' ? 'highlighted' : 'normal';
+    }
 
-	onShrink() {
-		this.wildState = 'shrunken';
-	}
+  onShrink() {
+      this.wildState = 'shrunken';
+    }
 
-	onDelete(item) {
-		this.list.splice(this.list.indexOf(item), 1);
-	}
-}
+  onDelete(item) {
+      this.list.splice(this.list.indexOf(item), 1);
+    }
+  }
