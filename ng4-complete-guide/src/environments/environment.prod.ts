b017259b4fe9294b5f77dcf534ca56dcc878cbd1
Nikita Starostin <n.starostin@itransition.com>
2017-07-13 11:36:08 +0300

    deploy https://ng-recipe-book-73a68.firebaseapp.com

--- ng4-complete-guide/src/environments/environment.prod.ts
+++ ng4-complete-guide/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCwichxD6Yvm-UluzYAR_ku6SE63QgrW6c',
+    authDomain: 'ng-recipe-book-73a68.firebaseapp.com',
+    databaseUrl: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+  }
 };
