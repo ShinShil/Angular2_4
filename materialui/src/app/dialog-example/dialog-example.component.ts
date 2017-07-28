104cb9191f4d574be88b1431e96773ea90e00a46
Nikita Starostin <n.starostin@itransition.com>
2017-07-28 11:21:18 +0300

    material hello world

---
+++ materialui/src/app/dialog-example/dialog-example.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-example',
+  template: `
+    <p>
+      dialog-example Works!
+    </p>
+  `,
+  styles: []
+})
+export class DialogExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
